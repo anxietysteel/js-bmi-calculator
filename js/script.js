23bea27bce11035d790428fb72151bd1d0649376
Stanley <thestr1keacc@gmail.com>
2025-09-09 21:28:27 +0500

    some final changes

--- js/script.js
+++ js/script.js
@@ -25,10 +25,10 @@ const translations = {
     calculateBtn: `Посчитать`,
     backBtn: `Назад`,
     annotation: `Этот калькулятор не заменяет консультацию врача. Для индивидуальных рекомендаций обратитесь к специалисту`,
-    underweight: `ИМТ меньше 18.5 — Недостаточный вес\nЭто может говорить о том, что организму не хватает питательных веществ. Иногда это связано с особенностями обмена веществ или образом жизни. Если такой показатель сочетается с усталостью, слабостью или частыми болезнями — стоит обсудить это с врачом.`,
-    normal: `ИМТ от 18.5 до 24.9 — Нормальный вес\nЭто считается здоровым диапазоном. Обычно он говорит о том, что масса тела и рост находятся в балансе. Конечно, на здоровье влияют и другие факторы (питание, физическая активность, сон), но этот показатель обычно считается благоприятным.`,
-    overweight: `ИМТ от 25 до 29.9 — Избыточный вес\nЭто может означать, что масса тела выше нормы. Для некоторых людей это связано с мышечной массой (например, у спортсменов), но часто это сигнал, что стоит обратить внимание на питание и уровень активности.`,
-    obesity: `ИМТ больше либо равно 30 — Ожирение\nПри таких значениях могут повышаться риски для здоровья (сердечно-сосудистые заболевания, диабет и т. д.). Важно воспринимать это как повод задуматься о корректировке образа жизни и, по возможности, проконсультироваться с врачом.`,
+    underweight: `ИМТ меньше 18.5 — Недостаточный вес. Это может говорить о том, что организму не хватает питательных веществ. Иногда это связано с особенностями обмена веществ или образом жизни. Если такой показатель сочетается с усталостью, слабостью или частыми болезнями — стоит обсудить это с врачом.`,
+    normal: `ИМТ от 18.5 до 24.9 — Нормальный вес. Это считается здоровым диапазоном. Обычно он говорит о том, что масса тела и рост находятся в балансе. Конечно, на здоровье влияют и другие факторы (питание, физическая активность, сон), но этот показатель обычно считается благоприятным.`,
+    overweight: `ИМТ от 25 до 29.9 — Избыточный вес. Это может означать, что масса тела выше нормы. Для некоторых людей это связано с мышечной массой (например, у спортсменов), но часто это сигнал, что стоит обратить внимание на питание и уровень активности.`,
+    obesity: `ИМТ больше либо равно 30 — Ожирение. При таких значениях могут повышаться риски для здоровья (сердечно-сосудистые заболевания, диабет и т. д.). Важно воспринимать это как повод задуматься о корректировке образа жизни и, по возможности, проконсультироваться с врачом.`,
   },
   en: {
     userBmi: `Your BMI`,
@@ -39,10 +39,10 @@ const translations = {
     calculateBtn: `Calculate`,
     backBtn: `Back`,
     annotation: `This calculator does not replace medical advice. For personalized recommendations, please consult a healthcare professional.`,
-    underweight: `BMI is less than 18.5 — Underweight\nThis may indicate that the body lacks essential nutrients. Sometimes it is related to metabolism or lifestyle. If this result is accompanied by fatigue, weakness, or frequent illnesses, it’s worth discussing with a doctor.`,
-    normal: `BMI from 18.5 to 24.9 — Normal weight\nThis is considered a healthy range. It usually means that body weight and height are in balance. Of course, health also depends on other factors (nutrition, physical activity, sleep), but this range is generally favorable.`,
-    overweight: `BMI from 25 to 29.9 — Overweight\nThis may mean that body weight is above normal. For some people, it may be due to muscle mass (e.g., athletes), but often it is a sign that diet and activity levels should be reviewed.`,
-    obesity: `BMI greater than or equal 30 — Obesity\nAt these levels, health risks may increase (cardiovascular diseases, diabetes, etc.). It’s important to treat this as a reason to consider lifestyle adjustments and, if possible, consult a doctor.`,
+    underweight: `BMI is less than 18.5 — Underweight. This may indicate that the body lacks essential nutrients. Sometimes it is related to metabolism or lifestyle. If this result is accompanied by fatigue, weakness, or frequent illnesses, it’s worth discussing with a doctor.`,
+    normal: `BMI from 18.5 to 24.9 — Normal weight. This is considered a healthy range. It usually means that body weight and height are in balance. Of course, health also depends on other factors (nutrition, physical activity, sleep), but this range is generally favorable.`,
+    overweight: `BMI from 25 to 29.9 — Overweight. This may mean that body weight is above normal. For some people, it may be due to muscle mass (e.g., athletes), but often it is a sign that diet and activity levels should be reviewed.`,
+    obesity: `BMI greater than or equal 30 — Obesity. At these levels, health risks may increase (cardiovascular diseases, diabetes, etc.). It’s important to treat this as a reason to consider lifestyle adjustments and, if possible, consult a doctor.`,
   },
 };
 
@@ -59,23 +59,23 @@ bmiForm.addEventListener("submit", (event) => {
 
   if (bmi < 18.5) {
     category = "underweight";
-    result.textContent =
-      `${translations[currentLang].userBmi} = ${bmi.toFixed(1)}\n` +
+    result.innerHTML =
+      `<p class="user-bmi">${translations[currentLang].userBmi} = ${bmi.toFixed(1)}</p>` +
       translations[currentLang].underweight;
   } else if (bmi >= 18.5 && bmi <= 24.9) {
     category = "normal";
-    result.textContent =
-      `${translations[currentLang].userBmi} = ${bmi.toFixed(1)}\n` +
+    result.innerHTML =
+      `<p class="user-bmi">${translations[currentLang].userBmi} = ${bmi.toFixed(1)}</p>` +
       translations[currentLang].normal;
   } else if (bmi >= 25 && bmi <= 29.9) {
     category = "overweight";
-    result.textContent =
-      `${translations[currentLang].userBmi} = ${bmi.toFixed(1)}\n` +
+    result.innerHTML =
+      `<p class="user-bmi">${translations[currentLang].userBmi} = ${bmi.toFixed(1)}</p>` +
       translations[currentLang].overweight;
   } else if (bmi >= 30) {
     category = "obesity";
-    result.textContent =
-      `${translations[currentLang].userBmi} = ${bmi.toFixed(1)}\n` +
+    result.innerHTML =
+      `<p class="user-bmi">${translations[currentLang].userBmi} = ${bmi.toFixed(1)}</p>` +
       translations[currentLang].obesity;
   }
 
@@ -85,21 +85,21 @@ bmiForm.addEventListener("submit", (event) => {
 });
 
 function toggleLang() {
-  if (category == "underweight") {
-    result.textContent =
-      `${translations[currentLang].userBmi} = ${BMI}\n` +
+  if (category == "underweight" && !result.textContent == '') {
+    result.innerHTML =
+      `<p class="user-bmi">${translations[currentLang].userBmi} = ${BMI}</p>` +
       translations[currentLang].underweight;
-  } else if (category == "normal") {
-    result.textContent =
-      `${translations[currentLang].userBmi} = ${BMI}\n` +
+  } else if (category == "normal" && !result.textContent == '') {
+    result.innerHTML =
+      `<p class="user-bmi">${translations[currentLang].userBmi} = ${BMI}</p>` +
       translations[currentLang].normal;
-  } else if (category == "overweight") {
-    result.textContent =
-      `${translations[currentLang].userBmi} = ${BMI}\n` +
+  } else if (category == "overweight" && !result.textContent == '') {
+    result.innerHTML =
+      `<p class="user-bmi">${translations[currentLang].userBmi} = ${BMI}</p>` +
       translations[currentLang].overweight;
-  } else if (category == "obesity") {
-    result.textContent =
-      `${translations[currentLang].userBmi} = ${BMI}\n` +
+  } else if (category == "obesity" && !result.textContent == '') {
+    result.innerHTML =
+      `<p class="user-bmi">${translations[currentLang].userBmi} = ${BMI}</p>` +
       translations[currentLang].obesity;
   }
 
